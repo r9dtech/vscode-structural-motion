41c26d5747768cbc96acb8ceacefc9e95c754fe4
R9dtech <r9dtech@gmail.com>
2024-06-08 22:41:22 +0000

    Parse incoming types to vscode classes

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!editor)
 			return;
 		const result = await vscode.commands.executeCommand('vscode.executeSelectionRangeProvider', editor.document.uri, [editor.selection.active])
-		let selectionRange: SelectionRange | undefined = parseSelectionRange(result);
+		let selectionRange: vscode.SelectionRange | undefined = parseSelectionRange(result);
 		while (selectionRange) {
 			vscode.window.showInformationMessage(JSON.stringify(selectionRange.range));
 			if (isFullLineSelection(selectionRange.range, editor)) {
@@ -18,10 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!selectionRange) {
 			return;
 		}
-		editor.selections = [new vscode.Selection(
-			new vscode.Position(selectionRange.range.start.line, selectionRange.range.start.character),
-			new vscode.Position(selectionRange.range.end.line, selectionRange.range.end.character),
-		)];
+		editor.selections = [new vscode.Selection(selectionRange.range.start, selectionRange.range.end)];
 	});
 
 	context.subscriptions.push(disposable);
@@ -30,43 +27,36 @@ export function activate(context: vscode.ExtensionContext) {
 const Position$ = z.object({
 	line: z.number(),
 	character: z.number(),
-});
+}).transform(p => new vscode.Position(p.line, p.character));
 
-const Range$ = z.object({
-	start: Position$,
-	end: Position$,
-});
 
-type Range = z.infer<typeof Range$>;
 const BaseSelectionRange$ = z.object({
-	range: Range$,
+	range: z.object({
+		start: Position$,
+		end: Position$,
+	}).transform(r => new vscode.Range(r.start, r.end)),
 });
 
-type SelectionRange = z.infer<typeof BaseSelectionRange$> & {
-	parent?: SelectionRange;
+type SelectionRangeInput = z.input<typeof BaseSelectionRange$> & {
+	parent?: SelectionRangeInput;
 };
 
-const SelectionRange$: z.ZodType<SelectionRange> = BaseSelectionRange$.extend({
+const SelectionRange$: z.ZodType<vscode.SelectionRange, z.ZodTypeDef, SelectionRangeInput> = BaseSelectionRange$.extend({
 	parent: z.optional(z.lazy(() => SelectionRange$)),
-})
+}).transform(sr=>new vscode.SelectionRange(sr.range, sr.parent));
 
 const SelectionRangeResult$ = z.tuple([SelectionRange$]);
 
-function parseSelectionRange(data: unknown): SelectionRange {
+function parseSelectionRange(data: unknown): vscode.SelectionRange {
 	return SelectionRangeResult$.parse(data)[0]; // TODO: what about if there are zero or many selection ranges?
 }
 
-
-function isFullLineSelection(range: Range, editor: vscode.TextEditor) {
-	if (range.start.character !== 0) {
-		return false;
-	}
-	const line = editor.document.lineAt(range.end.line);
+function isFullLineSelection(range: vscode.Range, editor: vscode.TextEditor) {
 	return (
-		range.end.character === line.range.end.character
-		||
-		range.end.character === line.rangeIncludingLineBreak.end.character
-	)
+		range.contains(editor.document.lineAt(range.start.line).range)
+		&&
+		range.contains(editor.document.lineAt(range.end.line).range)
+	);
 }
 
 export function deactivate() { }
