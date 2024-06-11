a0ad500c689313eef8cc93f0cd280a05966bdb1a
R9Dtech <r9dtech@gmail.com>
2024-06-11 10:56:55 +0000

    Simplify code - remove zod
    It turns out the command is already returning the right types - I previously thought it was POD

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,34 @@
+import { assert } from 'console';
 import * as vscode from 'vscode';
-import * as z from 'zod';
 
 export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('structural-motion.moveStructureUp', async () => {
         const editor = vscode.window.activeTextEditor;
+
         if (!editor) {
             return;
         }
+
         const documentUri = editor.document.uri;
         const documentVersion = editor.document.version;
         const originalCursorPosition = editor.selection.active;
-        const result = await vscode.commands.executeCommand('vscode.executeSelectionRangeProvider', documentUri, [
+
+        let selectionRange: vscode.SelectionRange | undefined = await getSelectionRanges(
+            documentUri,
             originalCursorPosition,
-        ]);
-        const originalSelectionRange = parseSelectionRange(result);
-        let selectionRange: vscode.SelectionRange | undefined = originalSelectionRange;
+        );
+
         while (selectionRange) {
             if (isFullLineSelection(selectionRange.range, editor)) {
                 break;
             }
             selectionRange = selectionRange.parent;
         }
-        if (!selectionRange) {
+
+        if (!selectionRange || selectionRange.range.start.line < 1) {
             return;
         }
-        if (selectionRange.range.start.line < 1) {
-            return;
-        }
+
         const moveRange = selectionRange.range.union(
             editor.document.lineAt(selectionRange.range.end.line).rangeIncludingLineBreak,
         );
@@ -45,36 +47,15 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-const Position$ = z
-    .object({
-        line: z.number(),
-        character: z.number(),
-    })
-    .transform((p) => new vscode.Position(p.line, p.character));
-
-const BaseSelectionRange$ = z.object({
-    range: z
-        .object({
-            start: Position$,
-            end: Position$,
-        })
-        .transform((r) => new vscode.Range(r.start, r.end)),
-});
-
-type SelectionRangeInput = z.input<typeof BaseSelectionRange$> & {
-    parent?: SelectionRangeInput;
-};
-
-const SelectionRange$: z.ZodType<vscode.SelectionRange, z.ZodTypeDef, SelectionRangeInput> = BaseSelectionRange$.extend(
-    {
-        parent: z.optional(z.lazy(() => SelectionRange$)),
-    },
-).transform((sr) => new vscode.SelectionRange(sr.range, sr.parent));
-
-const SelectionRangeResult$ = z.tuple([SelectionRange$]);
-
-function parseSelectionRange(data: unknown): vscode.SelectionRange {
-    return SelectionRangeResult$.parse(data)[0]; // TODO: what about if there are zero or many selection ranges?
+async function getSelectionRanges(uri: vscode.Uri, position: vscode.Position): Promise<vscode.SelectionRange> {
+    const result = await vscode.commands.executeCommand<vscode.SelectionRange[]>(
+        'vscode.executeSelectionRangeProvider',
+        uri,
+        [position],
+    );
+    assert(result instanceof Array && result.length === 1);
+    assert(result[0] instanceof vscode.SelectionRange);
+    return result[0];
 }
 
 function isFullLineSelection(range: vscode.Range, editor: vscode.TextEditor) {
